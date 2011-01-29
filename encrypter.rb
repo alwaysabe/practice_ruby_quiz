036c9595c5255a5ca7c0783d315bd3c6bac89537
Abe Sam Thomas <alwaysabe@gmail.com>
2011-01-29 16:17:15 +0530

    refactor: encrypter calss

--- encrypter.rb
+++ encrypter.rb
@@ -1,66 +1,61 @@
 class Encrypter
+  attr_accessor :cipher_text
 
-    def sanitise(str)
-        str = str.upcase.gsub(/[^A-Z]/,"")
-        return str
-    end
-    def pack5(str)
-        count=1
-        pack_str=""
-        str.each_char do |x|
-            if count%5 ==0 && count !=0
-                pack_str = pack_str+x+" "
-            else 
-                pack_str = pack_str+x
-            end
-        count+=1
-        end
-        return pack_str.strip
-    end
-    def to_num(message)
-        num_message=[]
-        message.each_char do |char|
-            num_message.push(char[0]-64) if char[0] !=32
-        end
-        return num_message
-    end
-    def combiner(value, key)
-        encrypted_value =[]
-        if value.size == key.size
-            for  i in 0..(value.size - 1)
-                if value[i] + key[i] > 26
-                    encrypted_value.push(value[i] + key[i] - 26)
-                else
-                    encrypted_value.push(value[i] + key[i])
-                end
-            end
-        end
-        return encrypted_value
-    end
-    def encrypt_data(data, key)
-        
-        sanitised = sanitise(data)
-        packed = pack5(sanitised)
-        value_num = to_num(packed)
-        key_num = to_num(pack5(key))
-        if value_num.size == key_num.size
-            encrypted_num = combiner(value_num, key_num)     
-        end
-        encrypted_string_tmp = ''
-        encrypted_num.each do |x|
-            encrypted_string_tmp = encrypted_string_tmp+(x+64).chr
-        end
-        encrypted_string = pack5(encrypted_string_tmp)
-        puts encrypted_string_tmp
-        return encrypted_string
+  def sanitize(str)
+    str.upcase.gsub(/[^A-Z]/, '')
+  end
 
+  def pack(str)
+    str += 'X' * ((str.size / 5.0).ceil * 5 - str.size)
+  end
+
+  def to_numbers(message)
+    numbers = []
+    message.each_byte { |c| numbers << c - 64 unless c == 32 }
+    numbers
+  end
+
+  def combiner(value, key)
+    encrypted = []
+    return encrypted unless value.size == key.size
+    value.size.times do |i|
+      e = value[i] + key[i]
+      e -= 26 if e > 26
+      encrypted << e
     end
-    def decrypt_data()
+    encrypted
+  end
+
+  def numbered(msg)
+    self.to_numbers(self.pack(self.sanitize(msg)))
+  end
+
+  def encrypt(data, key)
+    data_numbered = self.numbered(data)
+    key_numbered = self.numbered(key)
+    @cipher_text = self.combiner(data_numbered, key_numbered).map { |n| (n + 64).chr }.join
+    self.to_s
+  end
+
+  def decrypt(key)
+    # decrypt -> @cipher_text
+  end
+  
+  def to_s
+    packed_text = self.pack(@cipher_text)
+    s = ''
+    segments = (packed_text.size / 5.0).ceil
+    segments.times do |i|
+      s += "#{packed_text[i*5, 5]} "
     end
+    s.strip
+  end
 end
-sample_key = 'AAAAA AAAAA AA'
-ss="hello world"
-ee=Encrypter.new
-#puts ee.pack5(ss)
-#puts ee.to_num('ABCDE ZYXWV')
-puts ee.encrypt_data('abe sam thomas',sample_key)
+
+if __FILE__ == $0 then
+  sample_key = 'AAAAA AAAAA AA'
+  ss="hello world"
+  ee=Encrypter.new
+  puts ee.encrypt('abe sam thomas',sample_key)
+  puts ee.encrypt('Code in Ruby, live longer!', 'DWJXH YRFDG TMSHP UURXJ')
+end
